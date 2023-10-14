7d96dfbb46ec424ed2d364fccabb1d6cb809b6f0
chenbz35 <chenbz35@dimpt.com>
2023-10-14 12:07:02 +0800

    update nodejs and nginx

--- alpine-nodejs/build.sh
+++ alpine-nodejs/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build -t dimpt.site:8180/alpine-nodejs:18.18.0  --push .
+docker build -t dimpt.site:8180/alpine-nodejs:18.18.1  --push .
 
--- alpine-nodejs/buildx.sh
+++ alpine-nodejs/buildx.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-docker buildx build --platform=linux/amd64,linux/arm64 -t bingzic/alpine-nodejs:18.17.1 --push .
+#docker buildx build --platform=linux/amd64,linux/arm64 -t bingzic/alpine-nodejs:18.18.1 --push .
+docker buildx build --add-host dimpt.site:10.100.240.25 \
+    --platform=linux/amd64,linux/arm64 --provenance=false \
+    -t dimpt.site:8180/platform/alpine-nodejs:18.18.1 --push .
