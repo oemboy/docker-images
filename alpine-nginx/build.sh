718ad5b80d415eed5f80ab9ac40a4296db135e8e
bingzic <chenbz35@dimpt.com>
2023-10-15 23:35:35 +0800

    update shell

--- alpine-nginx/build.sh
+++ alpine-nginx/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build --add-host dimpt.site:10.100.240.25 -t platform/alpine-nginx:1.25.2 .
+docker build --add-host dimpt.site:10.100.240.25 -t bingzic/alpine-nginx:1.25.2 --push .
