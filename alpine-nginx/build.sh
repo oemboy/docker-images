24aa29459c9fd568241e8a6167110114f4a06551
bingzic <chenbz35@chinaunicom.cn>
2023-10-16 11:43:00 +0800

    update shell and dockerfile

--- alpine-nginx/build.sh
+++ alpine-nginx/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build --add-host dimpt.site:10.100.240.25 -t bingzic/alpine-nginx:1.25.2 --push .
+docker build --add-host dimpt.site:10.100.240.25 -t dimpt.site:8180/platform/alpine-nginx:1.25.2 --push .
