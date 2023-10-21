0654f4085d2e20f6ec801eb3bb6382553fb6ab3c
bingzic <chenbz35@dimpt.com>
2023-10-22 00:27:20 +0800

    update version

--- team-agent/build.sh
+++ team-agent/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build --add-host dimpt.site:10.100.240.25 -t dimpt.site:8180/platform/team-agent:2023.05.4-linux .
+docker build --add-host dimpt.site:10.100.240.25 -t dimpt.site:8180/platform/teamcity-minimal-agent:2023.05.4-linux .
