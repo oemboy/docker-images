12983cf53510ef93cfd1ff031e3a8258af50280a
bingzic <chenbz35@dimpt.com>
2023-10-29 11:36:43 +0800

    update nodejs

--- alpine-nodejs-20/buildx.sh
+++ alpine-nodejs-20/buildx.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-#docker buildx build --platform=linux/amd64,linux/arm64 -t dimpt.site:8180/platform/alpine-nodejs:16.20.0 .
-docker buildx build --add-host dimpt.site:10.100.240.69 --platform=linux/amd64,linux/arm64 -t bingzic/alpine-nodejs:16.20.0 . --push
+#docker buildx build --platform=linux/amd64,linux/arm64 -t dimpt.site:8180/platform/alpine-nodejs:20.9.0 .
+docker buildx build --add-host dimpt.site:10.100.240.69 --platform=linux/amd64,linux/arm64 -t bingzic/alpine-nodejs:20.9.0. --push
 
