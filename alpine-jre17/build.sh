6562b74c50660c5b95720acf9857674a91124b70
bingzic <chenbz35@dimpt.com>
2023-11-07 16:05:15 +0800

    update shell

--- alpine-jre17/build.sh
+++ alpine-jre17/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build --add-host dimpt.site:10.100.240.25 -t platform/alpine-jre17:17.0.9
+docker build --add-host dimpt.site:10.100.240.25 -t dimpt.site:8180/platform/alpine-jre17:17.0.9 --push .
