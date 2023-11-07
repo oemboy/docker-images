3e4cfce07a2f3b6628714a3b5ffbd56e7dfdde59
bingzic <chenbz35@chinaunicom.cn>
2023-11-07 15:03:26 +0800

    update version

--- alpine-jre17/build.sh
+++ alpine-jre17/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build -t platform/alpine-jre17:latest
+docker build --add-host dimpt.site:10.100.240.25 -t platform/alpine-jre17:17.0.9
--- alpine-jre21/build.sh
+++ alpine-jre21/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build --add-host dimpt.site:10.100.240.25 -t platform/alpine-jre11:latest
+docker build --add-host dimpt.site:10.100.240.25 -t platform/alpine-jre21:latest
