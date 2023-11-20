5a69a7ae0f3423eeac3155a7867927e7a0920efc
chenbingzi <chenbz35@dimpt.com>
2023-11-20 11:03:50 +0800

    add sh

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#mvn -Dfile.encoding=UTF-8 clean deploy -P docker-jdk8,docker-jdk11 -s /root/.m2/settings69.xml
+mvn -Dfile.encoding=UTF-8  --add-host dimpt.site:10.100.240.25 clean deploy -P team-agent -s /root/.m2/settings69.xml
--- team-agent/build.sh
+++ team-agent/build.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 docker build --add-host dimpt.site:10.100.240.25 -t dimpt.site:8180/platform/teamcity-minimal-agent:2023.05.4-linux .
+
