5017b05163c82ec6086aa9951ce2b43812affee8
bingzic <chenbz35@chinaunicom.cn>
2024-01-15 17:26:29 +0800

    delete redundant jre11

--- alpine-jre11-jammy/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-## ubuntu 的镜像地址是ubuntu-ports， 而不是ubuntu
-## http://mirrors.tuna.tsinghua.edu.cn/ubuntu-ports/
-## http://mirrors.tuna.tsinghua.edu.cn/ubuntu/
-docker build -t oemboy.site:8180/platform/alpine-jre11-jammy:latest . --push
