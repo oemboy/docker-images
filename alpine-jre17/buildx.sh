2b898937fb62d132fec38f1088b8325cdf963c22
bingzic <chenbz35@chinaunicom.cn>
2024-01-31 15:27:56 +0800

    update jre version

--- alpine-jre17/buildx.sh
+++ alpine-jre17/buildx.sh
@@ -2,4 +2,4 @@
 #docker buildx build --platform=linux/amd64,linux/arm64 -t bingzic/alpine-nodejs:18.18.2 --push .
 docker buildx build --add-host dimpt.site:10.100.240.25 \
     --platform=linux/amd64,linux/arm64 --provenance=false \
-    -t oemboy.site:8180/platform/openjdk-alpine:17.0.9-jre --push .
+    -t oemboy.site:8180/platform/openjdk-alpine:17.0.10-jre --push .
