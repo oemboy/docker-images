8124b5cc124f616e7e42028c7d0de5d84fb9317b
bingzic <chenbz35@chinaunicom.cn>
2024-02-22 16:36:58 +0800

    update mysql 8.0.36

--- mysql-debian/build.sh
+++ mysql-debian/build.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-docker build --add-host dimpt.site:10.100.240.25 -t dimpt.site:8180/platform/mysql-debian:8.0.35-bullseye .
-
+docker build --add-host dimpt.site:10.100.240.25 -t dimpt.site:8180/platform/mysql-debian:8.0.36-bullseye --push .
