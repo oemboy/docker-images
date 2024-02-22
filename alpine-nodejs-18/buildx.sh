83cd93e3583ed21f0fbb344525d32b141cb42470
bingzic <chenbz35@chinaunicom.cn>
2024-02-22 15:43:35 +0800

    update node 18.19.1 20.11.1

--- alpine-nodejs-18/buildx.sh
+++ alpine-nodejs-18/buildx.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 #docker buildx build --platform=linux/amd64,linux/arm64 -t bingzic/alpine-nodejs:18.19.1 --push .
-docker buildx build --add-host dimpt.site:10.100.240.25 \
-    --platform=linux/amd64,linux/arm64 --provenance=false \
+docker buildx build --platform=linux/amd64,linux/arm64 --provenance=false \
     -t dimpt.site:8180/platform/alpine-nodejs:18.19.1 --push .
--- alpine-nodejs-20/build.sh
+++ alpine-nodejs-20/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build --add-host dimpt.site:10.100.240.69 -t dimpt.site:8180/platform/alpine-nodejs:20.11.1  --push .
+docker build -t dimpt.site:8180/platform/alpine-nodejs:20.11.1  --push .
