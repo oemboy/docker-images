2118c2001bcab996dc9d4ab3c69f62699fa23ae5
bingzic <chenbz35@chinaunicom.cn>
2024-04-17 09:11:18 +0800

    update team-agent

--- team-agent/build.sh
+++ team-agent/build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-docker build --add-host dimpt.site:10.100.240.25 -t dimpt.site:8180/platform/teamcity-minimal-agent:2023.05.4-linux .
+docker build --add-host dimpt.site:10.100.240.25 \
+            --add-host gitlab.dimpt.site:10.10.46.2 \
+            -t dimpt.site:8180/platform/teamcity-minimal-agent:2024.03-linux --push .
 
