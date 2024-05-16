f7c8831fc0fb79292fe18a60133c297574765c3b
bingzic <chenbz35@dimpt.com>
2024-05-16 14:32:27 +0800

    add jenkins

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker build --add-host dimpt.site:10.100.240.25 --provenance=false \
+docker build --add-host dimpt.site:10.100.240.25 --add-host gitlab.dimpt.site:10.10.46.2 --provenance=false \
             -t dimpt.site:8180/platform/jenkins/jenkins:2.452.1-lts-jdk17 --push .
 
