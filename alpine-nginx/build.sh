f90e1ea4a28c27a4e575c0b4daff68c8d30a40b9
bingzic <chenbz35@chinaunicom.cn>
2024-05-31 15:57:10 +0800

    update nginx version

--- alpine-nginx/build.sh
+++ alpine-nginx/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build --add-host dimpt.site:10.100.240.25 -t dimpt.site:8180/platform/alpine-nginx:1.26.0 --push .
+docker build --add-host dimpt.site:10.100.240.25 -t dimpt.site:8180/platform/alpine-nginx:1.26.1 --push . --provenance=false
