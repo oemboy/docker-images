96d72af0cf94a86d49223e227841899d77bd6bb5
bingzic <chenbz35@chinaunicom.cn>
2024-05-31 16:58:11 +0800

    update openresty version

--- alpine-openresty/build.sh
+++ alpine-openresty/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build --add-host dimpt.site:10.100.240.25 -t dimpt.site:8180/platform/alpine-openresty:1.21.4.2 --push .
+docker build --add-host dimpt.site:10.100.240.25 -t dimpt.site:8180/platform/alpine-openresty:1.25.3.1 --push . --provenance=false
--- alpine-openresty/run.sh
+++ alpine-openresty/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker run -d -v /data/custom/conf.d:/etc/nginx/conf.d --name openresty \
-    dimpt.site:8181/platform/alpine-openresty:1.21.4.2
+    dimpt.site:8181/platform/alpine-openresty:1.25.3.1
