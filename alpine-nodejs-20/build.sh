04d33e410688616ab012688f20993173d2c05edc
bingzic <chenbz35@dimpt.com>
2024-07-11 17:00:13 +0800

    update node v20

--- alpine-nodejs-20/build.sh
+++ alpine-nodejs-20/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build -t dimpt.site:8180/platform/alpine-nodejs:20.12.2  --push .
-
+docker build -t dimpt.site:8180/platform/alpine-nodejs:20.15.1  --push .
+#docker build -t bingzic/alpine-nodejs:20.15.1 --push .
