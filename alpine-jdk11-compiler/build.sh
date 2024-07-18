59615393b8b69a8dfdd48e02c2304c56b300c003
bingzic <chenbz35@chinaunicom.cn>
2024-07-18 12:07:02 +0800

    update jdk version

--- alpine-jdk11-compiler/build.sh
+++ alpine-jdk11-compiler/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build --add-host dimpt.site:10.100.240.25 -t dimpt.site:8180/platform/alpine-jdk11:compiler --push .
+docker build --add-host dimpt.site:10.100.240.25 -t dimpt.site:8180/platform/alpine-jdk11-compiler:11.0.24 --push .
--- alpine-jdk8/build.sh
+++ alpine-jdk8/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build --add-host dimpt.site:10.100.240.25 -t dimpt.site:8180/platform/alpine-jdk8:8u412 --push .
+docker build --add-host dimpt.site:10.100.240.25 -t dimpt.site:8180/platform/alpine-jdk8:8u422 --push .
