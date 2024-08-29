7b74deded26b81eb3ab511f54547effb1b7a68b8
bingzic <chenbz35@chinaunicom.cn>
2024-08-29 15:53:06 +0800

    update nginx

--- alpine-nginx/buildx.sh
+++ alpine-nginx/buildx.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-docker buildx build --platform=linux/amd64,linux/arm64 -t bingzic/alpine-nginx:1.26.1 --push .
-#docker buildx build --add-host dimpt.site:10.100.240.25 --platform=linux/amd64,linux/arm64 -t oemboy.site:8180/platform/alpine-nginx:1.26.1 .
+docker buildx build --platform=linux/amd64,linux/arm64 -t bingzic/alpine-nginx:1.26.2 --push .
+#docker buildx build --add-host dimpt.site:10.100.240.25 --platform=linux/amd64,linux/arm64 -t oemboy.site:8180/platform/alpine-nginx:1.26.2 .
 #docker buildx build --add-host dimpt.site:10.100.240.25 \
 #    --platform=linux/amd64,linux/arm64 --provenance=false \
-#    -t dimpt.site:8180/platform/alpine-nginx:1.26.1 --push .
+#    -t dimpt.site:8180/platform/alpine-nginx:1.26.2 --push .
