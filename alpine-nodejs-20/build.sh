310e328c450b3e38386eca7d00c756cdb8c73094
bingzic <chenbz35@chinaunicom.cn>
2024-09-14 09:37:17 +0800

    update versions

--- alpine-nodejs-20/build.sh
+++ alpine-nodejs-20/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build -t dimpt.site:8180/platform/alpine-nodejs:20.15.1  --push .
-#docker build -t bingzic/alpine-nodejs:20.15.1 --push .
+docker build -t dimpt.site:8180/platform/alpine-nodejs:20.17.0  --push .
+#docker build -t bingzic/alpine-nodejs:20.17.0 --push .
--- mysql-debian/build.sh
+++ mysql-debian/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build --add-host dimpt.site:10.100.240.25 -t dimpt.site:8180/platform/mysql-debian:8.0.36-bullseye --push .
+docker build --add-host dimpt.site:10.100.240.25 -t dimpt.site:8180/platform/mysql-debian:8.4.2-bullseye --push .
