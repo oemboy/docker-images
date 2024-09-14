606512b9b770100f8f77307e621110a0ce883ed9
bingzic <chenbz35@chinaunicom.cn>
2024-09-14 10:26:10 +0800

    update versions

--- alpine-nodejs-18/buildx.sh
+++ alpine-nodejs-18/buildx.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#docker buildx build --platform=linux/amd64,linux/arm64 -t bingzic/alpine-nodejs:18.20.4--push .
+#docker buildx build --platform=linux/amd64,linux/arm64 -t bingzic/alpine-nodejs:18.20.4 --push .
 docker buildx build --platform=linux/amd64,linux/arm64 --provenance=false \
     -t dimpt.site:8180/platform/alpine-nodejs:18.20.4 --push .
 
