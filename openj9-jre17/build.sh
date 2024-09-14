6eb019321e202d83a0c3713f049d6e6286843a52
bingzic <chenbz35@chinaunicom.cn>
2024-09-14 09:49:15 +0800

    update versions

--- openj9-jre17/build.sh
+++ openj9-jre17/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker build --add-host dimpt.site:10.100.240.25 -t dimpt.site:8180/platform/openj9-jre17:17.0.9 . --push
+docker build --add-host dimpt.site:10.100.240.25 -t dimpt.site:8180/platform/openj9-jre17:17.0.12 . --push
 
 
