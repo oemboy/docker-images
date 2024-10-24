c45e081c56a036caa619defc6647ff0d4821ff44
bingzic <chenbz35@chinaunicom.cn>
2024-10-24 15:34:28 +0800

    update jdk version

--- alpine-jre11/build.sh
+++ alpine-jre11/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build --add-host dimpt.site:10.100.240.25 -t dimpt.site:8180/platform/alpine-jre11:11.0.24 --push .
+docker build --add-host dimpt.site:10.100.240.25 -t dimpt.site:8180/platform/alpine-jre11:11.0.25 --push .
