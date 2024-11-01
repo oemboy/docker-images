bdfea9a8c1a2d27d43317e0a49fb302dd0ed569c
bingzic <chenbz35@chinaunicom.cn>
2024-11-01 08:07:36 +0800

    del old version nodejs

--- alpine-nodejs-16/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-docker build --add-host dimpt.site:10.100.240.69 -t dimpt.site:8180/platform/alpine-nodejs:16.20.2  --push .
-
--- alpine-nodejs-16/buildx.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#docker buildx build --platform=linux/amd64,linux/arm64 -t dimpt.site:8180/platform/alpine-nodejs:16.20.2 .
-docker buildx build --add-host dimpt.site:10.100.240.69 --platform=linux/amd64,linux/arm64 -t bingzic/alpine-nodejs:16.20.2 . --push
-
