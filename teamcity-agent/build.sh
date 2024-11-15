042318314c41975e1eb22fa26f64c2a96e8d8f0c
bingzic <chenbz35@chinaunicom.cn>
2024-11-15 08:25:18 +0800

    update teamcity agent, support no root running.

--- teamcity-agent/build.sh
+++ teamcity-agent/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker build --add-host dimpt.site:10.100.240.25 \
             --add-host gitlab.dimpt.site:10.10.46.2 --provenance=false \
-            -t dimpt.site:8180/platform/teamcity-agent:2024.03.3-linux --push .
+            -t dimpt.site:8180/platform/teamcity-agent:2024.07.3-linux --push .
 
