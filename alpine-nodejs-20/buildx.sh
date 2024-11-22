9e5228400d67e4a8983c700da8107e96d2641871
bingzic <chenbz35@chinaunicom.cn>
2024-11-22 08:56:15 +0800

    update nodejs to 20.18.1

--- alpine-nodejs-20/buildx.sh
+++ alpine-nodejs-20/buildx.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#docker buildx build --platform=linux/amd64,linux/arm64 -t dimpt.site:8180/platform/alpine-nodejs:20.18.0 .
-docker buildx build --platform=linux/amd64,linux/arm64 -t bingzic/alpine-nodejs:20.18.0 --push .
+#docker buildx build --platform=linux/amd64,linux/arm64 -t dimpt.site:8180/platform/alpine-nodejs:20.18.1 .
+docker buildx build --platform=linux/amd64,linux/arm64 -t bingzic/alpine-nodejs:20.18.1 --push .
