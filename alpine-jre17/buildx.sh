460ed9f4a3d5dfa91a200bf31b70c1aab4710141
chenbz <chenbz35@chinaunicom.com>
2025-02-05 16:05:00 +0800

    update jdk17 and jdk21.

--- alpine-jre17/buildx.sh
+++ alpine-jre17/buildx.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker buildx build --add-host dimpt.site:10.100.240.25 \
     --platform=linux/amd64,linux/arm64 --provenance=false \
-    -t bingzic/alpine-jre17:17.0.13 --push .
+    -t bingzic/alpine-jre17:17.0.14 --push .
