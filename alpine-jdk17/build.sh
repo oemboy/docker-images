0c800d56cfa26af54d322abba382f8412793e9a1
chenbz <chenbz35@chinaunicom.com>
2025-04-28 15:04:56 +0800

    update jdk17 and jre17.

--- alpine-jdk17/build.sh
+++ alpine-jdk17/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build --add-host dimpt.site:10.100.240.25 -t dimpt.site:8180/platform/alpine-jdk17:17.0.14 --push .
+docker build --add-host dimpt.site:10.100.240.25 -t dimpt.site:8180/platform/alpine-jdk17:17.0.15 --push .
