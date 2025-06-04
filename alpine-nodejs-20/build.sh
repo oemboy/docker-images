b0d345cebd0c3f4e7ac6a7bf507eced96ae26d87
chenbz <chenbz35@chinaunicom.com>
2025-06-04 14:25:00 +0800

    update nodejs 20

--- alpine-nodejs-20/build.sh
+++ alpine-nodejs-20/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build -t dimpt.site:8180/platform/alpine-nodejs:20.19.1  --push .
-#docker build -t bingzic/alpine-nodejs:20.19.1 --push .
+docker build -t dimpt.site:8180/platform/alpine-nodejs:20.19.2  --push .
+#docker build -t bingzic/alpine-nodejs:20.19.2 --push .
