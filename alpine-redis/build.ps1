eb81a8bd577a2db480769607c457782bab6e3b07
bingzic <chenbz35@chinaunicom.cn>
2025-11-25 11:18:36 +0800

    update redis 6.2.21 update ENV GOSU_VERSION=1.19fix

---
+++ alpine-redis/build.ps1
@@ -0,0 +1,5 @@
+docker build `
+  --add-host dimpt.site:10.100.240.25 `
+  --provenance=false `
+  -t dimpt.site:8180/platform/alpine-redis:6.2.21 `
+  --push .
--- alpine-redis/build.sh
+++ alpine-redis/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker  build --add-host dimpt.site:10.100.240.25 -t dimpt.site:8180/platform/alpine-redis:6.2.14 --push .
+docker  build --add-host dimpt.site:10.100.240.25 -t dimpt.site:8180/platform/alpine-redis:6.2.21 --push .
