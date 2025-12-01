50995cc5d25ef7cb90fe933fed9b6123f0cc3cc3
bingzic <bingzic@163.com>
2025-12-01 15:50:21 +0800

    add support for -u

---
+++ alpine-zookeeper/docker-entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+# 设置默认 ZK_HOME
+#: ${ZK_HOME:=/opt/zookeeper}
+
+# 如果以 root 运行 zkServer.sh，则降权
+if [[ "$1" = 'zkServer.sh' && "$(id -u)" = '0' ]]; then
+    # 只修复需要写入的目录
+    chown -R devops:devops "$ZK_HOME"
+    # 重新执行自身，但以 devops 用户
+    exec su-exec devops "$0" "$@"
+fi
+
+# 正常执行命令
+exec "$@"
