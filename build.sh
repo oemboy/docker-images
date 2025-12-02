f826e24ee5118d0b85c1d8fda8daa85bd7a3afa9
bingzic <bingzic@163.com>
2025-12-02 15:38:50 +0800

    update docker maven plugin

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 #mvn -Dfile.encoding=UTF-8 clean deploy -P docker-jdk8,docker-jdk11 -s /root/.m2/settings69.xml
-mvn -Dfile.encoding=UTF-8  --add-host dimpt.site:10.100.240.25 clean deploy -P team-agent -s /root/.m2/settings69.xml
+#mvn -Dfile.encoding=UTF-8  --add-host dimpt.site:10.100.240.25 clean deploy -P team-agent -s /root/.m2/settings69.xml
+mvnd clean deploy -P alpine-jdk17,alpine-jre17
