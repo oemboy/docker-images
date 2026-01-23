e851c69aeb9d1ceb4c4ebabc3480205bfeb83a4a
bingzic <bingzic@163.com>
2026-01-23 16:38:59 +0800

    optimize alpine zookeeper

--- alpine-zookeeper/docker-entrypoint.sh
+++ alpine-zookeeper/docker-entrypoint.sh
@@ -1,11 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 set -e
-
 # 设置默认 ZK_HOME
 #: ${ZK_HOME:=/opt/zookeeper}
 
 # 如果以 root 运行 zkServer.sh，则降权
-if [[ "$1" = 'zkServer.sh' && "$(id -u)" = '0' ]]; then
+if [ "$1" = 'zkServer.sh' ] && [ "$(id -u)" = '0' ]; then
     # 只修复需要写入的目录
     chown -R devops:devops "$ZK_HOME"
     # 重新执行自身，但以 devops 用户
