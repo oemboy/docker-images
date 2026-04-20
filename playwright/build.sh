5b8ab1426deeeb75fa8f1a2b11ad3c5087e443d1
bingzic <bingzic@163.com>
2026-04-20 14:37:56 +0800

    update playwright

--- playwright/build.sh
+++ playwright/build.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-docker build --add-host dimpt.site:10.100.240.25 -t dimpt.site:8180/platform/playwright:v1.58.0-noble-jdk21 --push .
+docker build --add-host dimpt.site:10.100.240.25 -t dimpt.site:8180/platform/playwright:v1.59.0-noble-jdk21 --push .
+docker tag  dimpt.site:8180/platform/playwright:v1.59.0-noble-jdk21 registry.cn-guangzhou.aliyuncs.com/platform2022/playwright:v1.59.0-noble-jdk21
+docker push registry.cn-guangzhou.aliyuncs.com/platform2022/playwright:v1.59.0-noble-jdk21
