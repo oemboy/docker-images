927b3155bf050f8ce3f149778facc319c7f31b73
bingzic <bingzic@163.com>
2026-05-07 12:05:31 +0800

    add elasticsearch

---
+++ elasticsearch/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+docker build --add-host dimpt.site:10.100.240.25 \
+            --add-host gitlab.dimpt.site:10.10.46.2 --provenance=false \
+            -t dimpt.site:8180/platform/elasticsearch:9.3.4 --push .
+
