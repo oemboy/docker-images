b25438d1853f7c179721e78d49b5718d22bacfa5
bingzic <bingzic@163.com>
2026-05-12 09:36:31 +0800

    update elasticsearch version

--- elasticsearch/build.sh
+++ elasticsearch/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker build --add-host dimpt.site:10.100.240.25 \
             --add-host gitlab.dimpt.site:10.10.46.2 --provenance=false \
-            -t dimpt.site:8180/platform/elasticsearch:9.3.4 --push .
+            -t dimpt.site:8180/platform/elasticsearch:9.4.0 --push .
