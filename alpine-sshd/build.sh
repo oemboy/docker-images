5c8abdff726fcdbd8cd041cdb847f7da0b888a27
bingzic <bingzic@163.com>
2026-05-28 09:25:51 +0800

    update alpine version

--- alpine-sshd/build.sh
+++ alpine-sshd/build.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-docker build --build-arg PASSWORD=admin -t bingzic/alpine-sshd:latest --push .
+#docker build --build-arg PASSWORD=admin -t bingzic/alpine-sshd:latest --push .
+docker build --build-arg PASSWORD=admin -t dimpt.site:8180/platform/alpine-sshd:latest --push .
