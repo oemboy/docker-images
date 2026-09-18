bb37808cf57b836a396902ce644a8aed02227eaa
bingzic <bingzic@163.com>
2026-09-18 16:20:28 +0800

    update node version

--- alpine-nodejs-24/buildx.sh
+++ alpine-nodejs-24/buildx.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#docker buildx build --platform=linux/amd64,linux/arm64 -t dimpt.site:8180/platform/alpine-nodejs:24.16.0 .
-docker buildx build --platform=linux/amd64,linux/arm64 -t bingzic/alpine-nodejs:24.16.0 --push .
+#docker buildx build --platform=linux/amd64,linux/arm64 -t dimpt.site:8180/platform/alpine-nodejs:24.21 .
+docker buildx build --platform=linux/amd64,linux/arm64 -t bingzic/alpine-nodejs:24.21 --push .
