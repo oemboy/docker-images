40ab258e8b4053e4a5b87d15bac18df5bfbf29d6
bingzic <bingzic@163.com>
2026-09-23 10:47:20 +0800

    multi platform for jdk11, redis8, zookeeper

--- alpine-redis8/build.sh
+++ alpine-redis8/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker  build --add-host dimpt.site:10.100.240.25 -t dimpt.site:8180/platform/alpine-redis:8.8.2 --push .
+docker  build --add-host dimpt.site:10.100.240.25 -t dimpt.site:8180/platform/alpine-redis:8.2.10 --push .
