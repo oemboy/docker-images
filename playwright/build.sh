683b59f5b293739a4e40c8f8a6403b897d8d65d7
bingzic <bingzic@163.com>
2026-09-24 09:33:34 +0800

    multi platform for playwright

--- playwright/build.sh
+++ playwright/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker build --add-host dimpt.site:10.100.240.25 -t dimpt.site:8180/platform/playwright:v1.59.0-noble-jdk21 --push .
-docker tag  dimpt.site:8180/platform/playwright:v1.59.0-noble-jdk21 registry.cn-guangzhou.aliyuncs.com/platform2022/playwright:v1.59.0-noble-jdk21
-docker push registry.cn-guangzhou.aliyuncs.com/platform2022/playwright:v1.59.0-noble-jdk21
+docker build --add-host dimpt.site:8180/platform/playwright:v1.63.0-jdk25 --push .
+docker tag  dimpt.site:8180/platform/playwright:v1.63.0-jdk25 registry.cn-guangzhou.aliyuncs.com/platform2022/playwright:v1.63.0-jdk25
+docker push registry.cn-guangzhou.aliyuncs.com/platform2022/playwright:v1.63.0-jdk25
